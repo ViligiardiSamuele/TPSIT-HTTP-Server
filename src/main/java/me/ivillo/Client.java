e4960491ffcd55b7bcf01063ebf7179f747bb231
lai2-24 <lai2-24@itismeucci.com>
2023-12-18 10:26:20 +0100

    wip

--- src/main/java/me/ivillo/Client.java
+++ src/main/java/me/ivillo/Client.java
@@ -3,7 +3,9 @@ package me.ivillo;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.time.LocalDateTime;
@@ -11,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Client extends Thread{
+public class Client extends Thread {
 
     private Boolean enable;
     private Socket socket;
@@ -19,7 +21,7 @@ public class Client extends Thread{
     private DataOutputStream out;
     private List<String> buffer;
 
-    public Client(Socket socket){
+    public Client(Socket socket) {
         this.enable = true;
         this.socket = socket;
         try {
@@ -34,32 +36,38 @@ public class Client extends Thread{
     @Override
     public void run() {
         String input;
+        StringBuilder sb = new StringBuilder("-- From [" + socket.getPort() + "] -----\n");
         try {
-            while(enable && in.ready()){
+            while (enable && in.ready()) {
                 input = in.readLine();
                 buffer.add(input);
-                if(input.equals(""))
+                if (input.equals("")) {
                     execute();
+                    System.out.println(sb.append("-------------\n\n").toString());
+                    sb.delete(0, sb.length());
+                    sb.append("\n\n--" + socket.getPort() + "------\n");
+                } else sb.append(input + "\n");
+
             }
             out.close();
             in.close();
             socket.close();
-            System.out.println(socket.getPort() + " ha interrotto la connessione");
+            System.out.println(socket.getPort() + ": Ha interrotto la connessione");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void execute(){
-        String[] string0 = buffer.get(0).split("\\s+",0);
+    private void execute() throws IOException {
+        String[] string0 = buffer.get(0).split("\\s+", 0);
         File file = new File("htdocs" + string0[1]);
-        if(file.exists()){
-            if(file.isDirectory()){ //ERRORE PATH
-                System.out.println("Invio di: " + file.getPath() + "/index.html");
-                sendFile(new File(file.getPath() + "/index.html"), "text/html");
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                System.out.println(socket.getPort() + ": Invio di: " + file.getPath() + "/index.html" + " (" + getContentType(file) + ")");
+                sendBinaryFile(out, new File(file.getPath() + "/index.html"));
             } else {
-                System.out.println("Invio di: " + file.getPath());
-                sendFile(file, "text/html");
+                System.out.println(socket.getPort() + ": Invio di: " + file.getPath() + " (" + getContentType(file) + ")");
+                sendBinaryFile(out, file);
             }
         } else {
             System.out.println("File non trovato: " + file.getPath());
@@ -68,7 +76,7 @@ public class Client extends Thread{
                 out.writeBytes("HTTP/1.1 404 Not Found\n");
                 out.writeBytes("Date: " + LocalDateTime.now().toString() + "\n");
                 out.writeBytes("Server: meucci-server\n");
-                out.writeBytes("Content-Type: text/html; charset=UTF-8\n");
+                out.writeBytes("Content-Type: text/html\n");
                 out.writeBytes("Content-Length: " + msg.length() + "\n");
                 out.writeBytes("\n");
                 out.writeBytes(msg + "\n");
@@ -78,21 +86,52 @@ public class Client extends Thread{
         }
     }
 
-    private void sendFile(File file, String type){
+    private void sendFile(File file) {
         try {
             out.writeBytes("HTTP/1.1 200 OK\n");
             out.writeBytes("Date: " + LocalDateTime.now().toString() + "\n");
             out.writeBytes("Server: meucci-server\n");
-            out.writeBytes("Content-Type: "+ type +"; charset=UTF-8\n");
+            out.writeBytes("Content-Type: " + getContentType(file) + "\n");
             out.writeBytes("Content-Length: " + file.length() + "\n");
             out.writeBytes("\n");
             Scanner fileReader = new Scanner(file);
-            while(fileReader.hasNextLine()){
+            while (fileReader.hasNextLine()) {
                 out.writeBytes(fileReader.nextLine() + "\n");
             }
             fileReader.close();
         } catch (Exception e) {
             e.printStackTrace();
-        }    
+        }
+    }
+
+    private String getContentType(File f) {
+        String[] s = f.getName().split("\\.");
+        String ext = s[s.length - 1];
+        switch (ext) {
+            case "html":
+            case "htm":
+                return "text/html";
+            case "png":
+                return "image/png";
+            case "css":
+                return "text/css";
+            default:
+                return "";
+        }
+
+    }
+
+    private void sendBinaryFile(DataOutputStream output, File file) throws IOException {
+        output.writeBytes("HTTP/1.1 200 OK\n");
+        output.writeBytes("Content-Length: " + file.length() + "\n");
+        output.writeBytes("Content-Type: " + getContentType(file) + "\n");
+        output.writeBytes("\n");
+        InputStream input = new FileInputStream(file);
+        byte[] buf = new byte[8192];
+        int n;
+        while ((n = input.read(buf)) != -1) {
+            output.write(buf, 0, n);
+        }
+        input.close();
     }
 }
